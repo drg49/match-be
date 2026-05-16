ea0e652cee3e758dc00ded75e0abeb1e609776cf
Danny Gavin <dan.gavin49@gmail.com>
2026-05-15 23:55:24 -0700

    Remove reservation

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,11 +2,9 @@ from flask import Blueprint
 
 # Import the route handlers from other files
 from .authentication import authentication
-from .reservation import reservation
 
 # Create the blueprints
 routes = Blueprint('routes', __name__)
 
 # Register the blueprints with the Flask app
 routes.register_blueprint(authentication, url_prefix='/authentication')
-routes.register_blueprint(reservation, url_prefix='/reservation')
