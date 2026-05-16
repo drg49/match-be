31e72cd2fc10f0d7886d06f97dbd0d141dd6264e
Danny Gavin <dan.gavin49@gmail.com>
2026-05-16 01:37:12 -0700

    Refactor Users model to enhance structure and clarity

--- models/user.py
+++ models/user.py
@@ -1,16 +1,30 @@
 from models import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
-# In the context of this application, the term "user" pertains exclusively to individuals employed within the restaurant establishment. 
-# It is noteworthy that customers are not required to register or establish accounts within this system.
 class Users(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    # Primary Key
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(25), nullable=False)
-    last_name = db.Column(db.String(25), nullable=False)
+
+    # Auth
     email = db.Column(db.String(150), nullable=False, unique=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
     password = db.Column(db.String(105), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    bio = db.Column(db.String(1000), nullable=False, default='')
+    birthdate = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(50))
+    interested_in = db.Column(db.String(50))
+    height_cm = db.Column(db.Integer)
+    location = db.Column(db.String(100))
+    # Account Status
+    is_active = db.Column(db.Boolean, default=True)
+    is_verified = db.Column(db.Boolean, default=False)
 
     def get_id(self):
         return str(self.id)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
